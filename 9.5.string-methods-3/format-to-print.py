5d7ba5b6b4bf138d6d6fe888ff2495c11fa81720
Beregszászi István <beistvan@gmail.com>
2023-07-15 06:47:46 +0200

    task 9.5

---
+++ 9.5.string-methods-3/format-to-print.py
@@ -0,0 +1,5 @@
+year = 2023
+amount = '10K'
+currency = 'Bitcoin'
+
+print('In {}, someone paid {} {} for two pizzas.'.format(year, amount, currency))
---
+++ 9.5.string-methods-3/format-to-string.py
@@ -0,0 +1,3 @@
+s = 'In {0}, someone paid {1} {2} for two pizzas.'
+txt = s.format("2023", "10k", "Bitcoin")
+print(txt)
