ba83b2619f482509d9515d962ca60767daa76a1d
Beregszászi István <beistvan@gmail.com>
2023-07-22 06:05:59 +0200

    task 13.1

---
+++ 13.1.func-no-param/star-rectangle.py
@@ -0,0 +1,9 @@
+# function's declaration
+def draw_box():
+    print('*'*10)
+    for _ in range(12):
+        print("*" + (" "*8) + "*")
+    print('*'*10)
+
+# main program
+draw_box()  # function's call
---
+++ 13.1.func-no-param/star-triangle-1.py
@@ -0,0 +1,7 @@
+# declaration of function
+def draw_triangle():
+    for i in range(1, 11):
+        print('*'*i)
+
+# main prorgam part
+draw_triangle()  # call of function
