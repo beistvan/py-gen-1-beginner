b967b782b712f937be31c132e2602ff17a35f206
beistvan <beistvan@gmail.com>
2025-04-06 00:51:34 +0200

    8.2.3.code-review-9.py

---
+++ 8.2.final-loops/8.2.3.code-review-9.py
@@ -0,0 +1,16 @@
+n = 4
+count = 0
+maximum = -float('inf')
+
+for i in range(1, n + 1):
+    x = int(input())
+    if x % 2 != 0:
+        count += 1
+        if x > maximum:
+            maximum = x
+
+if count > 0:
+    print(count)
+    print(maximum)
+else:
+    print('NO')
